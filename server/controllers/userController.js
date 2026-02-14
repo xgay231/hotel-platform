3d498c4d8e3e35723b6770e0c8192b34196a7e93
xgay231 <xgay231@gmail.com>
2026-02-14 18:12:34 +0800

    feat(server): 更新用户注册功能，使用 bcryptjs 加密密码

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,5 @@
 const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
   try {
@@ -9,9 +10,12 @@ exports.register = async (req, res) => {
       return res.status(400).json({ message: "Username already exists" });
     }
 
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
     const user = await User.create({
       username,
-      password,
+      password: hashedPassword,
     });
 
     res.status(201).json({
