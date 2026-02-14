6d0d94b75197cf48f8e8e94434a8eb4866a3a41e
xgay231 <xgay231@gmail.com>
2026-02-14 18:46:16 +0800

    feat(server): 实现用户登录功能及相关测试用例

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -32,3 +32,32 @@ exports.register = async (req, res) => {
     });
   }
 };
+
+exports.login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username });
+    if (!user) {
+      return res.status(401).json({ message: "账号或密码错误" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(401).json({ message: "账号或密码错误" });
+    }
+
+    res.json({
+      message: "登录成功",
+      user: {
+        id: user._id,
+        username: user.username,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
--- server/routes/users.js
+++ server/routes/users.js
@@ -4,4 +4,6 @@ const userController = require("../controllers/userController");
 
 router.post("/register", userController.register);
 
+router.post("/login", userController.login);
+
 module.exports = router;
